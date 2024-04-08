5e66a0b4891f8a1cc3b49cc1b482eb154ea7b391
Szeng89 <shijinz898@gmail.com>
2024-04-08 00:37:41 -0400

    started first test

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,19 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void addTest() {
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        String str = "zipcode";
+        list.add(str);
+
+        Assert.assertEquals(str, list.getClass(0));
+    }
+
 }
